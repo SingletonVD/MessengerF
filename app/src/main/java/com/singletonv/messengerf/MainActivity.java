67b2a26b3581b0d1774bd18a5f4925b45e20f1a2
SingletonVD <singletonvd@mail.ru>
2025-10-06 01:09:40 +0300

    Init

---
+++ app/src/main/java/com/singletonv/messengerf/MainActivity.java
@@ -0,0 +1,13 @@
+package com.singletonv.messengerf;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
