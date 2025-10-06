300d6888b5b801af371cc0a74b1d7c29f4242c60
SingletonVD <singletonvd@mail.ru>
2025-10-06 21:22:51 +0300

    feat: add chat layout

---
+++ app/src/main/java/com/singletonv/messengerf/ChatActivity.java
@@ -0,0 +1,34 @@
+package com.singletonv.messengerf;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private TextView textViewTitle;
+    private View viewUserStatus;
+    private RecyclerView recyclerViewMessages;
+    private EditText editTextMessage;
+    private ImageView imageViewSendMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        initViews();
+    }
+
+    private void initViews() {
+        textViewTitle = findViewById(R.id.textViewTitle);
+        viewUserStatus = findViewById(R.id.viewUserStatus);
+        recyclerViewMessages = findViewById(R.id.recyclerViewMessages);
+        editTextMessage = findViewById(R.id.editTextMessage);
+        imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
+    }
+}
